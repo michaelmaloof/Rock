3e08ad2430ec33633f6c483379aa3aa5d18383b2
Michael Maloof <michaelmaloof1991@gmail.com>
2015-02-04 20:03:50 -0500

    constraints

---
+++ DerivedData/RockPet/Build/Intermediates/RockPet.build/Debug-iphoneos/RockPet.build/Objects-normal/arm64/ViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/MichaelMaloof/Documents/Development/RockPet/RockPet/ViewController.m \
+  /Users/MichaelMaloof/Documents/Development/RockPet/RockPet/ViewController.h
--- RockPet/ViewController.m
+++ RockPet/ViewController.m
@@ -9,19 +9,89 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property NSDate *firstDate;
+@property NSDate *currentDate;
+@property NSDateFormatter *formatter;
+@property float daysSinceFirstDate;
+@property BOOL usedBefore;
+@property float newHeight;
+@property float newWidth;
+@property (weak, nonatomic) IBOutlet UIImageView *rock;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"sup";
+    
+    //set date format to year, month, and day
+    self.formatter = [[NSDateFormatter alloc] init];
+    [self.formatter setDateFormat: @"yyyy-MM-dd"];
+    
+    //load the bool for if the user has used the app or not
+    NSUserDefaults *use = [NSUserDefaults standardUserDefaults];
+    self.usedBefore = [use integerForKey:@"used"];
+    NSLog(@"used before? %ld,", (long)self.usedBefore);
+    self.newWidth = 40;
+    self.newHeight = 40;
+    
+    [self adjustRockSize];
+    
+}
+
+-(void)adjustRockSize
+{
+    //actions to do if the user has or has not used the app
+    if (self.usedBefore == 0)
+    {
+        //turn today's date into a string and save it
+        self.firstDate = [NSDate date];
+        NSString *stringFromDate = [self.formatter stringFromDate:self.firstDate];
+        NSUserDefaults *firstDateDefault = [NSUserDefaults standardUserDefaults];
+        [firstDateDefault setObject: stringFromDate forKey:@"firstDate"];
+        
+        //turn self.usedBefore bool to YES
+        NSUserDefaults *use= [NSUserDefaults standardUserDefaults];
+        [use setBool:1 forKey:@"used"];
+        
+    }
+    
+    
+    if(self.usedBefore == 1)
+    {
+        //load users first date on the app and convert from string to date
+        NSUserDefaults *firstDateDefault = [NSUserDefaults standardUserDefaults];
+        NSString *firstDateString = [firstDateDefault stringForKey:@"firstDate"];
+        self.firstDate = [self.formatter dateFromString:firstDateString];
+        NSLog(@"first date = %@", self.firstDate);
+        
+        //find time between user's first date and today
+        self.currentDate = [NSDate date];
+        NSLog(@"current date = %@", self.currentDate);
+        self.daysSinceFirstDate = [self.currentDate timeIntervalSinceDate:self.firstDate]/86400;
+        NSLog(@"days since= %f", self.daysSinceFirstDate);
+        
+        //adjust size of rock
+        self.newHeight = self.newHeight + (self.daysSinceFirstDate);
+        NSLog(@"height = %f", self.newHeight);
+        self.newWidth = self.newWidth + (self.daysSinceFirstDate);
+        NSLog(@"width = %f", self.newWidth);
+        self.rock.frame = CGRectMake(
+                                     self.rock.frame.origin.x,
+                                     self.rock.frame.origin.y, self.newWidth, self.newHeight);
+        NSLog(@"%@", NSStringFromCGRect(self.rock.frame));
+        
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
+
+
 
 @end
