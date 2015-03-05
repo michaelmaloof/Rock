898aebece6f063fc5a3c73157797f58223589b17
Archon Light <mike@altri.us>
2015-03-04 21:36:31 -0500

    Update view controller with new code

--- RockPet/ViewController.m
+++ RockPet/ViewController.m
@@ -8,15 +8,18 @@
 
 #import "ViewController.h"
 
+
+static float const initialWidth = 40;
+static float const initialHeight = 40;
+
 @interface ViewController ()
+
 @property NSDate *firstDate;
-@property NSDate *currentDate;
 @property NSDateFormatter *formatter;
 @property float daysSinceFirstDate;
-@property BOOL usedBefore;
-@property float newHeight;
-@property float newWidth;
 @property (weak, nonatomic) IBOutlet UIImageView *rock;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *rockHeight;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *rockWidth;
 
 @end
 
@@ -25,69 +28,78 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.title = @"sup";
+    
+    //Use NSLocalizedString to handle multiple languages
+    self.title = NSLocalizedString(@"sup","sup");
     
     //set date format to year, month, and day
     self.formatter = [[NSDateFormatter alloc] init];
     [self.formatter setDateFormat: @"yyyy-MM-dd"];
     
-    //load the bool for if the user has used the app or not
-    NSUserDefaults *use = [NSUserDefaults standardUserDefaults];
-    self.usedBefore = [use integerForKey:@"used"];
-    NSLog(@"used before? %ld,", (long)self.usedBefore);
-    self.newWidth = 40;
-    self.newHeight = 40;
+    //Rather than load a boolen value to see if the user has run the app before,
+    //just load the first date and check for nil. If nil, it hasn't been run before,
+    //so, save the current date as the first date and continue
+    [self loadFirstDateDefault];
     
+    //Adjust the size of the rock from the initial width and height based on
+    //your timeInterval that you already wrote.
     [self adjustRockSize];
     
 }
 
--(void)adjustRockSize
+#pragma mark - Methods
+-(void)loadFirstDateDefault
 {
-    //actions to do if the user has or has not used the app
-    if (self.usedBefore == 0)
-    {
-        //turn today's date into a string and save it
-        self.firstDate = [NSDate date];
-        NSString *stringFromDate = [self.formatter stringFromDate:self.firstDate];
-        NSUserDefaults *firstDateDefault = [NSUserDefaults standardUserDefaults];
-        [firstDateDefault setObject: stringFromDate forKey:@"firstDate"];
-        
-        //turn self.usedBefore bool to YES
-        NSUserDefaults *use= [NSUserDefaults standardUserDefaults];
-        [use setBool:1 forKey:@"used"];
-        
-    }
-    
-    
-    if(self.usedBefore == 1)
-    {
-        //load users first date on the app and convert from string to date
-        NSUserDefaults *firstDateDefault = [NSUserDefaults standardUserDefaults];
-        NSString *firstDateString = [firstDateDefault stringForKey:@"firstDate"];
+    //load users first date on the app and convert from string to date
+    NSUserDefaults *firstDateDefault = [NSUserDefaults standardUserDefaults];
+    NSString *firstDateString = [firstDateDefault stringForKey:@"firstDate"];
+    if(firstDateString == (id)[NSNull null] || firstDateString.length == 0 ){
+        [self setFirstDateDefault];
+    }else{
         self.firstDate = [self.formatter dateFromString:firstDateString];
-        NSLog(@"first date = %@", self.firstDate);
-        
-        //find time between user's first date and today
-        self.currentDate = [NSDate date];
-        NSLog(@"current date = %@", self.currentDate);
-        self.daysSinceFirstDate = [self.currentDate timeIntervalSinceDate:self.firstDate]/86400;
-        NSLog(@"days since= %f", self.daysSinceFirstDate);
-        
-        //adjust size of rock
-        self.newHeight = self.newHeight + (self.daysSinceFirstDate);
-        NSLog(@"height = %f", self.newHeight);
-        self.newWidth = self.newWidth + (self.daysSinceFirstDate);
-        NSLog(@"width = %f", self.newWidth);
-        self.rock.frame = CGRectMake(
-                                     self.rock.frame.origin.x,
-                                     self.rock.frame.origin.y, self.newWidth, self.newHeight);
-        NSLog(@"%@", NSStringFromCGRect(self.rock.frame));
-        
+        NSLog(@"loaded first date = %@", self.firstDate);
     }
 }
 
-- (void)didReceiveMemoryWarning {
+-(void)setFirstDateDefault
+{
+    //turn today's date into a string and save it
+    //The firstDate won't persist using NSUserDefaults. If the user deletes the app
+    //and then reinstalls, they will need to start over at 40 X 40
+    //If you want the firstDate to persist through deletes, use the keychain
+    self.firstDate = [NSDate date];
+    NSString *stringFromDate = [self.formatter stringFromDate:self.firstDate];
+    NSUserDefaults *firstDateDefault = [NSUserDefaults standardUserDefaults];
+    [firstDateDefault setObject: stringFromDate forKey:@"firstDate"];
+    NSLog(@"saved first date = %@", self.firstDate);
+}
+
+-(void)adjustRockSize
+{
+    //find time between user's first date and today
+    self.daysSinceFirstDate = [[NSDate date] timeIntervalSinceDate:self.firstDate]/86400;
+    NSLog(@"Rock is %f day(s) old", self.daysSinceFirstDate);
+    
+    //perform size adjustment
+    self.rockWidth.constant = [self updateRockWidth];
+    self.rockHeight.constant = [self updateRockHeight];
+        
+}
+
+#pragma mark - Calculations
+-(float)updateRockWidth
+{
+    return initialWidth + self.daysSinceFirstDate;
+}
+
+-(float)updateRockHeight
+{
+    return initialHeight + self.daysSinceFirstDate;
+}
+
+#pragma mark - Memory Warnings
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
 }
 
